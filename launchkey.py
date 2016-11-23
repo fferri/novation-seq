f29cb03090c4fe984ba19ec4f4000d65ad413715
Federico Ferri <federico.ferri.it@gmail.com>
2016-11-23 19:54:13 +0100

    split dev proto (pd-agnostic), pd midi & pd class

--- launchkey.py
+++ launchkey.py
@@ -1,39 +1,57 @@
 import pyext
 
-class launchkey(pyext._class):
-    _inlets = 1
-    _outlets = 3
-
-    def __init__(self, *args):
-        self.midiBuffer = [[], []]
+class Launchkey(object):
+    def __init__(self):
+        self.portName = {0: 'MIDI', 1: 'InControl'}
+        self.reportUnrecognizedMessages = False
         self.buttonMidiMap = {0x68: 'track-up', 0x69: 'track-down',
                               0x6A: 'track-left', 0x6B: 'track-right'}
         self.controlMidiMap = {0x15 + i: i for i in range(8)}
 
-    def __del__(self):
-        pass
+    def onNoteEvent(self, note, velocity):
+        raise RuntimeError('method Launchkey.onXyzEvent not implemented')
+
+    def onPadEvent(self, row, col, velocity):
+        raise RuntimeError('method Launchkey.onXyzEvent not implemented')
+
+    def onControlEvent(self, num, value):
+        raise RuntimeError('method Launchkey.onXyzEvent not implemented')
+
+    def onButtonEvent(self, name, pressed):
+        raise RuntimeError('method Launchkey.onXyzEvent not implemented')
 
     def writeMidi(self, v1, v2, v3):
-        self._outlet(2, v1)
-        self._outlet(2, v2)
-        self._outlet(2, v3)
+        raise RuntimeError('method Launchkey.writeMidi not implemented')
 
-    def writeOutput(self, *l):
-        self._outlet(3, l)
-
-    def midi_1(self, f):
-        self.bufferMidi(0, int(f))
-
-    def cmidi_1(self, f):
-        self.bufferMidi(1, int(f))
+    def onMidiData(self, port, data):
+        if port == 0:
+            if data[0] & 0xF0 in (0x90, 0x80):
+                self.onNoteEvent(data[1], data[2])
+                return
+        elif port == 1:
+	    if data[0] in (0xB0, ):
+		if data[1] >= 0x15 and data[1] <= 0x1C:
+		    self.onControlEvent(self.controlMidiMap[data[1]], data[2])
+                    return
+		if data[1] >= 0x68 and data[1] <= 0x6D:
+		    self.onButtonEvent(self.buttonMidiMap[data[1]], data[2] > 0)
+                    return
+	    elif data[0] in (0x90, 0x80) and data[1] & 0xF0 in (0x60, 0x70) and data[1] & 0x0F <= 8:
+		row = int(data[1] & 0xF0 == 0x70)
+		col = data[1] & 0x0F
+		self.onPadEvent(row, col, data[2])
+                return
+        if self.reportUnrecognizedMessages:
+            fmt = 'Launchkey: unrecognized midi data on %s iface: %02X %02X %02X'
+            print(fmt % (self.portName[port]) + tuple(data))
 
     def clip(self, x, xmin, xmax):
         return max(xmin, min(xmax, x))
 
-    def reset_1(self):
+    def reset(self):
         self.writeMidi(0xB0, 0x00, 0x00)
 
-    def extendedmode_1(self, enable):
+    def setExtendedMode(self, enable):
         self.writeMidi(0x90, 0x0C, 0x7F * int(enable))
 
     def makeValue(self, r, g, clear=False, copy=False):
@@ -43,7 +61,7 @@ class launchkey(pyext._class):
         copy = int(copy)
         return r | (g << 4) | (clear << 3) | (copy << 2)
 
-    def setled_1(self, row, col, r, g, *args):
+    def setLed(self, row, col, r, g):
         row = int(self.clip(row, 0, 1))
         col = int(self.clip(col, 0, 8))
         r = int(self.clip(r, 0, 3))
@@ -52,30 +70,57 @@ class launchkey(pyext._class):
         val = (r & 3) | ((g & 3) << 4)
         self.writeMidi(status, addr, val)
 
-    def bufferMidi(self, i, f):
-        if f & 0x80: # status bit
-            self.midiBuffer[i] = [f]
-        else:
-            self.midiBuffer[i].append(f)
-        if len(self.midiBuffer[i]) == 3:
-            self.onMidiData(i, self.midiBuffer[i])
-            self.midiBuffer[i] = []
+class LaunchkeyPdImpl(Launchkey):
+    def __init__(self, pdobj):
+        super(LaunchkeyPdImpl, self).__init__()
+        self.pdobj = pdobj
+        self.midiBuffer = [[], []]
 
-    def onMidiData(self, i, data):
-        if i == 0:
-            if data[0] & 0xF0 in (0x90, 0x80):
-                self.writeOutput('note', data[1], data[2])
-            else:
-                print('launchkey: unrecognized midi on note iface: %02X %02X %02X' % tuple(data))
-        elif i == 1:
-	    if data[0] in (0xB0, ):
-		if data[1] >= 0x15 and data[1] <= 0x1C:
-		    self.writeOutput('control', self.controlMidiMap[data[1]], data[2])
-		if data[1] >= 0x68 and data[1] <= 0x6D:
-		    self.writeOutput('button', self.buttonMidiMap[data[1]], data[2] > 0)
-	    elif data[0] in (0x90, 0x80) and data[1] & 0xF0 in (0x60, 0x70) and data[1] & 0x0F <= 8:
-		row = int(data[1] & 0xF0 == 0x70)
-		col = data[1] & 0x0F
-		self.writeOutput('pad', row, col, data[2])
-	    else:
-		print('launchkey: unrecognized midi on control iface: %02X %02X %02X' % tuple(data))
+    def writeMidi(self, v1, v2, v3):
+        self.pdobj._outlet(2, v1)
+        self.pdobj._outlet(2, v2)
+        self.pdobj._outlet(2, v3)
+
+    def bufferMidi(self, port, f):
+        if f & 0x80: # status bit
+            self.midiBuffer[port] = [f]
+        else:
+            self.midiBuffer[port].append(f)
+        if len(self.midiBuffer[port]) == 3:
+            self.onMidiData(port, self.midiBuffer[port])
+            self.midiBuffer[port] = []
+
+    def onNoteEvent(self, note, velocity):
+        self.pdobj._outlet(3, ['note', note, velocity])
+
+    def onPadEvent(self, row, col, velocity):
+        self.pdobj._outlet(3, ['pad', row, col, velocity])
+
+    def onControlEvent(self, num, value):
+        self.pdobj._outlet(3, ['control', num, value])
+
+    def onButtonEvent(self, name, pressed):
+        self.pdobj._outlet(3, ['button', name, pressed])
+
+class LaunchkeyPd(pyext._class):
+    _inlets = 1
+    _outlets = 3
+
+    def __init__(self, *args):
+        self.launchkey = LaunchkeyPdImpl(self)
+
+    def midi_1(self, f):
+        self.launchkey.bufferMidi(0, int(f))
+
+    def cmidi_1(self, f):
+        self.launchkey.bufferMidi(1, int(f))
+
+    def reset_1(self):
+        self.launchkey.reset()
+
+    def extendedmode_1(self, enable):
+        self.launchkey.setExtendedMode(enable)
+
+    def setled_1(self, row, col, r, g, *args):
+        self.launchkey.setLed(row, col, r, g)
+
--- launchpad.py
+++ launchpad.py
@@ -1,30 +1,25 @@
 import pyext
 
-class launchpad(pyext._class):
-    _inlets = 1
-    _outlets = 2
-
-    def __init__(self, *args):
-        self.midiBuffer = []
-
-    def __del__(self):
-        pass
+class Launchpad(object):
+    def onButtonEvent(self, row, col, pressed):
+        raise RuntimeError('method Launchpad.onButtonEvent not implemented')
 
     def writeMidi(self, v1, v2, v3):
-        self._outlet(1, v1)
-        self._outlet(1, v2)
-        self._outlet(1, v3)
+        raise RuntimeError('method Launchpad.writeMidi not implemented')
 
-    def writeOutput(self, *l):
-        self._outlet(2, l)
-
-    def midi_1(self, f):
-        self.bufferMidi(int(f))
+    def onMidiData(self, data):
+        if data[0] == 0xB0:
+            row = 8
+            col = data[1] - 104
+        else:
+            row = data[1] // 16
+            col = data[1] % 16
+        self.onButtonEvent(row, col, data[2] > 0)
 
     def clip(self, x, xmin, xmax):
         return max(xmin, min(xmax, x))
 
-    def reset_1(self):
+    def reset(self):
         self.writeMidi(0xB0, 0x00, 0x00)
 
     def makeValue(self, r, g, clear=False, copy=False):
@@ -34,22 +29,18 @@ class launchpad(pyext._class):
         copy = int(copy)
         return r | (g << 4) | (clear << 3) | (copy << 2)
 
-    def setled_1(self, row, col, r, g, *args):
+    def setLed(self, row, col, r, g, clear=False, copy=False):
         row = int(self.clip(row, 0, 8))
         col = int(self.clip(col, 0, 8))
         r = int(self.clip(r, 0, 3))
         g = int(self.clip(g, 0, 3))
-        clear = bool(args[0]) if len(args) >= 1 else False
-        copy = bool(args[1]) if len(args) >= 2 else False
         if row == 8: status, addr = 0xB0, 0x68 + col # top row
         else: status, addr = 0x90, (row & 0x0F) << 4 | 0x0F & col
         self.writeMidi(status, addr, self.makeValue(r, g, clear, copy))
 
-    def buffers_1(self, displaying, updating, *args):
+    def setBuffers(self, displaying, updating, flash=False, copy=False):
         displaying = int(self.clip(displaying, 0, 1))
         updating = int(self.clip(updating, 0, 1))
-        flash = int(bool(args[0])) if len(args) >= 1 else False
-        copy = int(bool(args[1])) if len(args) >= 2 else False
         val = 1 << 5
         val |= displaying
         val |= updating << 2
@@ -57,24 +48,34 @@ class launchpad(pyext._class):
         val |= copy << 4
         self.writeMidi(0xB0, 0x00, val)
 
-    def testleds_1(self, val):
+    def testLeds(self, val):
         self.writeMidi(0xB0, 0x00, 0x7D + self.clip(val, 0, 2))
 
-    def setdutycycle_1(self, num, denom):
+    def setDutyCycle(self, num, denom):
         num = int(self.clip(num, 1, 16))
         denom = int(self.clip(denom, 3, 18))
         h = int(num >= 9)
         self.writeMidi(0xB0, 0x1E + h, 16 * (num - 1 - 8 * h) + denom - 3)
 
-    def setleds(self, *args):
+    def setLeds(self, *args):
         if len(args) not in (128, 144, 160):
-            print('setleds must have either 128, 144 or 160 arguments')
-        else:
-            for i in range(0, len(args), 4):
-                r1, g1, r2, g2 = map(int, args[i:i+4])
-                v1 = self.makeValue(r1, g1)
-                v2 = self.makeValue(r2, g2)
-                self.writeMidi(0x92, v1, v2)
+            raise ValueError('setleds must have either 128, 144 or 160 arguments')
+        for i in range(0, len(args), 4):
+            r1, g1, r2, g2 = map(int, args[i:i+4])
+            v1 = self.makeValue(r1, g1)
+            v2 = self.makeValue(r2, g2)
+            self.writeMidi(0x92, v1, v2)
+
+class LaunchpadPdImpl(Launchpad):
+    def __init__(self, pdobj):
+        super(LaunchpadPdImpl, self).__init__()
+        self.pdobj = pdobj
+        self.midiBuffer = []
+
+    def writeMidi(self, v1, v2, v3):
+        self.pdobj._outlet(1, v1)
+        self.pdobj._outlet(1, v2)
+        self.pdobj._outlet(1, v3)
 
     def bufferMidi(self, f):
         if f & 0x80: # status bit
@@ -85,11 +86,35 @@ class launchpad(pyext._class):
             self.onMidiData(self.midiBuffer)
             self.midiBuffer = []
 
-    def onMidiData(self, data):
-        if data[0] == 0xB0:
-            row = 8
-            col = data[1] - 104
-        else:
-            row = data[1] // 16
-            col = data[1] % 16
-        self.writeOutput('button', row, col, data[2] > 0)
+    def onButtonEvent(self, row, col, pressed):
+        self.pdobj._outlet(2, ['button', row, col, int(pressed)])
+    
+class LaunchpadPd(pyext._class):
+    _inlets = 1
+    _outlets = 2
+
+    def __init__(self):
+        self.launchpad = LaunchpadPdImpl(self)
+
+    def midi_1(self, f):
+        self.launchpad.bufferMidi(int(f))
+
+    def reset_1(self):
+        self.launchpad.reset()
+
+    def setled_1(self, row, col, r, g, *args):
+        clear = bool(args[0]) if len(args) >= 1 else False
+        copy = bool(args[1]) if len(args) >= 2 else False
+        self.launchpad.setLed(row, col, r, g, clear, copy)
+
+    def buffers_1(self, displaying, updating, *args):
+        flash = int(bool(args[0])) if len(args) >= 1 else False
+        copy = int(bool(args[1])) if len(args) >= 2 else False
+        self.launchpad.setBuffers(displaying, updating, flash, copy)
+
+    def testleds_1(self, val):
+        self.launchpad.testLeds(val)
+
+    def setdutycycle_1(self, num, denom):
+        self.launchpad.setDutyCycle(num, denom)
+
